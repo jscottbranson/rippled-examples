d332f3ac4e486b7c4f0f81e85c04ec9966323a98
crypticrabbit <the@rabbitkick.club>
2018-04-12 12:17:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# rippled-examples
+[rippled-examples] contains sample code that is used to interact with or configure rippled. Some of the code in this repo is written in JS, and requires [ripple-lib].
+
+##Synopsis
+This purpose of this repository is to store sample code. Since this is sample code, it is provided as is. You are responsible for how you use it. Users are encouraged to always specify a maximum fee, as well as the last validated ledger for the transaction. See [Reliable Transaction Submission] for additional information on submitting rippled transactions.
+
+##License
+Anyone is free to use, modify, and distribute any of these files.
+
+[rippled-examples]:https://github.com/crypticrabbit/rippled-examples
+[ripple-lib]:https://github.com/ripple/ripple-lib
+[Reliable Transaction Submission]:https://ripple.com/build/reliable-transaction-submission/
---
+++ trust_set.js
@@ -0,0 +1,61 @@
+//NOTE: This sample code is provided as is. It has not been tested. Use at your own risk.
+//You are the only person responsible for what you do with this code
+//
+//
+//ALSO NOTE: This code will not set the appropriate flag to prevent your balances from rippling.
+//if you trust more than one counterparty with IOUs, you risk your balances changing
+//without your control, unless you disable rippling on ALL trust lines
+
+
+'use strict';
+//use ripple-lib
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+
+//Your wallet info
+const address = 'Put your wallet address here';
+const secret = 'Put your secret here';
+
+//Other variables
+const fee = {
+	maxFee: '.000010'
+	};
+
+//Adjust this to the current ledger + ~4.
+//Ideally, use an API call to do this automatically
+const last_ledger = {
+		'LastLedgerSequence': 37910550
+	};
+
+
+//Rippled server - change as needed
+const api = new RippleAPI({server: 'wss//s1.ripple.com:443'});
+
+//Change the following settings according to your needs
+//limit is the most value you trust the counterparty to issue to you
+const settings = {
+		'currency': 'USD',
+		'counterparty': 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',
+		'limit': '1000'
+};
+	
+
+function quit(message) {
+  console.log(message);
+  process.exit(0);
+}
+
+function fail(message) {
+  console.error(message);
+  process.exit(1);
+}
+
+api.connect().then(() => {
+  console.log('Preparing the transaction...');
+  return api.prepareTrustline(address, settings, fee, last_ledger).then(prepared => {
+    console.log('Success! Transaction prepared...');
+    const {signedTransaction} = api.sign(prepared.txJSON, secret);
+    console.log('Transaction signed...');
+    api.submit(signedTransaction).then(quit, fail);
+  });
+}).catch(fail);
