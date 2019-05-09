935d6e120ef186c25a278a10eb4cb2f4963068a3
crypticrabbit <the@rabbitkick.club>
2019-05-08 22:51:07 -0400

    Fixed multiple style errors in and added order book example to websocket example.

--- rippled_websocket.py
+++ rippled_websocket.py
@@ -1,89 +1,110 @@
-# Code by rabbit:
-#   * https://rabbitkick.club or @rabbitkickclub on Twitter
+'''
+Code by rabbit:
+  * https://rabbitkick.club or @rabbitkickclub on Twitter
 
-# This script provides an example/template using python3 to connect to a rippled
-# node via websocket (WS). WS must also be configured in the node's rippled.cfg file.
-# This script uses the 'websocket' module, which is available via pip.
-# This script is based on this example: https://github.com/websocket-client/websocket-client
+This script provides an example/template using python3 to connect to a rippled
+node via websocket (WS). WS must also be configured in the node's rippled.cfg file.
+This script uses the 'websocket' module, which is available via pip.
+This script is based on this example: https://github.com/websocket-client/websocket-client
 
-# This is offered 'as is'. Users will likely need to modify the script to meet their
-# individual needs.
+This is offered 'as is'. Users will likely need to modify the script to meet their
+individual needs.
 
-# Subscriptions provide ongoing information on the XRP ledger's state, including transactions.
-# One use for this script is monitoring transaction flows or other data from the ledger.
+Subscriptions provide ongoing information on the XRP ledger's state, including transactions.
+One use for this script is monitoring transaction flows or other data from the ledger.
+'''
 
-import numpy  # makes websocket faster
-import websocket
+import sys
+import ssl # This is only used to disable wss cert verification.
 import json
-#import ssl #only need ssl to disable wss cert verification.
+
+import websocket # 'pip install websocket-client'
 
 # Specify the server to connect to
-ws_address = "wss://s1.ripple.com:443"
+WS_ADDRESS = "wss://s1.ripple.com:443"
 
 # Tailor subscriptions here
 # Subscriptions are described in the rippled documentation:
 # https://ripple.com/build/rippled-apis/#subscribe
-ledger = '{"id": "1", "command": "subscribe", "streams": ["ledger"]}'
-server = '{"id": "1", "command": "subscribe", "streams": ["server"]}'
-validations = '{"id": "1", "command": "subscribe", "streams": ["validations"]}'
+LEDGER = {"id": "1", "command": "subscribe", "streams": ["ledger"]}
+SERVER = {"id": "1", "command": "subscribe", "streams": ["server"]}
+VALIDATIONS = {"id": "1", "command": "subscribe", "streams": ["validations"]}
+BOOKS = {
+    "command": "subscribe",
+    "books": [
+        {
+            "taker_pays": {
+                "currency": "XRP"
+            },
+            "taker_gets": {
+                "currency": "USD",
+                "issuer": "rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq"
+            },
+            "snapshot": "true"
+        },
+        {
+            "taker_pays": {
+                "currency": "USD",
+                "issuer": "rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq"
+            },
+            "taker_gets": {
+                "currency": "XRP"
+            },
+            "snapshot": "true"
+        },
+        ]
+}
 
 # Use the following variable to define which subscription to use
-ws_command = ledger
+WS_COMMAND = json.dumps(BOOKS)
 
-# Change what information is displayed by adding code to the appropriate 'parse_xxxxx' methods'
-class ws:
+class Ws:
+    '''
+    Open a websocket connection, send the subscription command,
+    and parse messages from the web server.
+    '''
     def __init__(self):
-        self.command = ws_command
-        self.address = ws_address
-        self.record_txl_high = int()
+        self.websocket_launch()
 
-    def parse_ledger(self, message):
-        m = message
-        if m['txn_count'] > self.record_txl_high:
-            self.record_txl_high = m['txn_count']
-        print("Ledger ID:", m['ledger_index'], "contained", m['txn_count'], 'transactions.')
-        print("Record high tx/ledger:", self.record_txl_high)
-        print("-------------------------------------------")
-
-    def parse_server(self, message):
-        print(message)
-        print("-------------------------------------------")
-
-    def parse_validations(self, message):
-        print(message)
-        print("-------------------------------------------")
-
-    def on_message(self, ws, message):
+    def on_message(self, message):
         message = json.loads(message)
         #Decide which method to use to parse the message
-        if self.command == ledger:
-            self.parse_ledger(message)
-        elif self.command == server:
-            self.parse_server(message)
-        elif self.command == validations:
-            self.parse_validations(message)
+        print(message)
 
-    def on_error(self, ws, error):
+    def on_error(self, error):
         print("Error:", error)
 
-    def on_close(self, ws):
+    def on_close(self):
         print("WS connection closed.")
 
-    def on_open(self, ws):
-        ws.send(self.command)
+    def on_open(self):
+        '''
+        Initial command to send after opening the websocket.
+        '''
+        self.socket.send(WS_COMMAND)
 
     def websocket_launch(self):
-        ws = websocket.WebSocketApp(self.address,
-                                    on_message = self.on_message,
-                                    on_error = self.on_error,
-                                    on_close = self.on_close)
-        ws.on_open = self.on_open
+        '''
+        Construct and open the websocket connection.
+        To disable ssl verification:
+          1. Comment/remove the first 'ws.run_forever()' line below
+          2. Uncomment the second line that starts with 'ws.run_forever(sslopt=...'
+        '''
+        self.socket = websocket.WebSocketApp(
+            WS_ADDRESS,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+            on_open=self.on_open,
+        )
 
-        # To disable ssl verification:
-        #   1. Uncomment 'import ssl' at the top of this file
-        #   2. Uncomment the first line that starts with 'ws.run_forever(sslopt=...'
-        #   3. Comment/remove the second 'ws.run_forever()' line 
-        #ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
-        ws.run_forever()
+        try:
+            self.socket.run_forever()
+            #self.socket.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        except KeyboardInterrupt:
+            sys.exit()
 
-ws().websocket_launch()
+try:
+    Ws()
+except KeyboardInterrupt:
+    sys.exit()
